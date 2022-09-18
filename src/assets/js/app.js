c2c6a8c04b75c5104cc760f710c0f367dcc73944
annenko94 <annenko.anatoly94@gmail.com>
2022-09-18 15:17:22 +0300

    second commit

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,6 @@
-console.log("Test message");
+const ratingItemsList = document.querySelectorAll(".skills-list__rating-item");
+ratingItemsList.forEach (item =>
+    item.addEventListener("click",()=>
+    item.parentNode.dataset.totalValue = item.dataset.itemValue
+    )
+    );
