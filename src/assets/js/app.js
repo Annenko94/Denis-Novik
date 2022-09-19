0126a5b96dfaf014fdf07248d99b5e8b0254fbdd
annenko94 <annenko.anatoly94@gmail.com>
2022-09-19 13:31:25 +0300

    3 coomit

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,3 @@
-const ratingItemsList = document.querySelectorAll(".skills-list__rating-item");
-ratingItemsList.forEach (item =>
-    item.addEventListener("click",()=>
-    item.parentNode.dataset.totalValue = item.dataset.itemValue
-    )
-    );
+import Swiper from 'swiper/bundle';
+
+new Swiper('.image-slider');
