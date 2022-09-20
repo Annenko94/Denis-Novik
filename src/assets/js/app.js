81d543d5cbad06faed38475b140d4900476bf492
annenko94 <annenko.anatoly94@gmail.com>
2022-09-20 14:34:25 +0300

    header

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,22 @@
 import Swiper from 'swiper/bundle';
+const slider = document.querySelector('.image-slider');
 
-new Swiper('.image-slider');
+let mySwiper = new Swiper(slider,{
+    slidesPerView: 1.1,
+    navigation: {
+        nextEl: document.querySelector('.swiper-button-next'),
+        prevEl: document.querySelector('.swiper-button-prev')
+    },
+    pagination: {
+        el: document.querySelector('.swiper-pagination'),
+        clickable: true,
+    },
+    scrollbar: {
+        el: document.querySelector('swiper-scrollbar'),
+        draggable: true
+    },
+   
+});
+    
+        
+ 
